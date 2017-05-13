bfecd6e5b4380c77d3c1b0ea0ba99722552a6046
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2017-05-13 13:41:43 +0200

    Add last solutions for learnyounode

--- learnyounode/http-json-api-server.js
+++ learnyounode/http-json-api-server.js
@@ -3,39 +3,39 @@ const parse = require('querystring').parse;
 const url = require('url');
 
 function getTime(iso) {
-    const now = new Date(iso);
-    return {
-        hour: now.getHours(),
-        minute: now.getMinutes(),
-        second: now.getSeconds(),
-    };
+  const now = new Date(iso);
+  return {
+    hour: now.getHours(),
+    minute: now.getMinutes(),
+    second: now.getSeconds(),
+  };
 }
 
 function getUnixTime(iso) {
-    return { unixtime: Date.parse(iso) };
+  return { unixtime: Date.parse(iso) };
 }
 
 const server = http.createServer((req, res) => {
-    const reqURL = url.parse(req.url);
-    const path = reqURL.pathname;
-    const { iso } = parse(reqURL.query);
+  const reqURL = url.parse(req.url);
+  const path = reqURL.pathname;
+  const { iso } = parse(reqURL.query);
 
-    if (path === '/api/parsetime') {
-        const timeObj = getTime(iso);
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(timeObj));
-        return;
-    }
+  if (path === '/api/parsetime') {
+    const timeObj = getTime(iso);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(timeObj));
+    return;
+  }
 
-    if (path === '/api/unixtime') {
-        const timeObj = getUnixTime(iso);
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(timeObj));
-        return;
-    }
+  if (path === '/api/unixtime') {
+    const timeObj = getUnixTime(iso);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(timeObj));
+    return;
+  }
 
-    res.statusCode = 500;
-    res.end();
+  res.statusCode = 500;
+  res.end();
 });
 
 server.listen(process.argv[2]);
--- learnyounode/juggling-async.js
+++ learnyounode/juggling-async.js
@@ -1,3 +1,4 @@
+// @flow
 const http = require('http');
 
 let count = 0;
